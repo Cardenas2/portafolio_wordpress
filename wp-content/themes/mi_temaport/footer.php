ac6f855c0e0d749abd779b53a3dcfb1f3a0ef9db
Cardenas2 <vaniacardenas-21@hotmail.com>
2018-08-29 20:39:05 -0300

    crear paginas header.php y footer.php

--- wp-content/themes/mi_temaport/footer.php
+++ wp-content/themes/mi_temaport/footer.php
@@ -1,7 +1,32 @@
-<?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'header_menu' ) ); ?>
-<?php } ?>
+<footer class="footerPortafolio text-center">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 footer__iconos ml-auto mr-auto">
+				<a href="https://www.facebook.com/vania.e.cardenas"><i class="fab fa-facebook-f footer__iconos--ind"></i></a>
+				<a href="https://twitter.com/Vaniacardenas21"><i class="fab fa-twitter footer__iconos--ind2"></i></a>
+				<a href="https://www.linkedin.com/in/vania-c%C3%A1rdenas-36265216/"><i class="fab fa-linkedin-in footer__iconos--ind3"></i></i></a>
+			</div>
+			<div class="col-xs-12 col-sm-6 text-center footer__contactame">
+				<a href="contacto.html">Si te gusta o te interesa mi trabajo, Contactame!!!!!!</a>
+			</div>
+		</div><!--fin row footer-->
+	</footer>
 
-<?php wp_footer() ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+
+
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+
+
+
+<!-- or -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<?php wp_footer()?>
+
 </body>
 </html>
+
--- wp-content/themes/mi_temaport/header.php
+++ wp-content/themes/mi_temaport/header.php
@@ -1,14 +1,83 @@
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo('charset') ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php get_template_part('_includes/iOS', 'icons') ?>
-	<?php wp_head() ?>
+	<meta charset=<?php bloginfo('charset') ?>>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title><?php bloginfo(); ?></title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<?php wp_head()?>
 </head>
-<body>
+<body class="contenedor">
+	<nav class="menuPortafolio1 py-4">
 
-<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
-<?php } ?>
+		<a href="">
+			<div class="menuPortafolio1__logo ml-auto mr-auto">
+
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/log-vc.jpg" alt="logotipo portafolio">
+			</div>
+		</a>
+
+		<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+				'container'      => false,
+				'depth'          => 2,
+				'menu_class' => 'nav justify-content-center '
+				 
+			) );
+			?>
+		<!--<ul class="nav justify-content-center">
+			
+			<li class="nav-item">
+				<a class="nav-link active" href="index.html">PORTAFOLIO</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="sobre-mi.html">ALGO DE MI</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="blog.html">BLOG</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="contacto.html">CONTACTO</a>
+			</li>
+		</ul>-->
+	</nav>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light menuPortafolio2">
+		<a class="menuPortafolio2__logo" href="#">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/log-vc.jpg" alt="logotipo portafolio">
+		</a>
+		<button class="navbar-toggler icon-amburguesa" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon "></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+				'container'      => false,
+				'depth'          => 2,
+				
+				 
+			) );
+			?>
+			<!--<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link menuPortafolio2_btn" href="index.html">PORTAFOLIO<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link menuPortafolio2_btn" href="sobre-mi.html">ALGO DE MI</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link menuPortafolio2_btn" href="blog.html">BLOG</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link menuPortafolio2_btn" href="contacto.html">CONTACTO</a>
+				</li>
+			</ul>
+		</div>-->
+	</nav>
