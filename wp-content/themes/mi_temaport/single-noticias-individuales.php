f29817a602a649bdcd72df9043d20f06fc6dca6d
Cardenas2 <vaniacardenas-21@hotmail.com>
2018-09-03 14:24:49 -0300

    seccion noticias individuales

--- wp-content/themes/mi_temaport/single-noticias-individuales.php
+++ wp-content/themes/mi_temaport/single-noticias-individuales.php
@@ -2,28 +2,39 @@
 <?php get_header() ?>
 
 <?php if ( have_posts() ) { ?>
+
 	<?php while ( have_posts() ) { ?>
-		<section class="container-fluid portafolio2 py-5">
-		<div class="row portafolio__trabajos py-3 mx-4">
-			<div class="col-xs-12 col-sm-6 col-md-6 text-left py-5">
-				<?php the_post(); ?>
+		<section class="container noticias">
+		<div class="row">
+			<div class="col-sm-12 my-3">
+				<?php the_post_thumbnail('noticias_individuales') ?>
+			</div>
+			<div class="col-sm-12">
 				<h2><?php the_title(); ?></h2>
 				<p>
 					<?php the_content() ?>
 				</p>
-				<a class="btn btn-primary mr-auto" href="http://localhost:8888/portafolio/" role="button">Volver</a>
 			</div>
-			<div class="col-xs-12 col-sm-6 col-md-6">
-				<?php the_post_thumbnail('trabajos-individuales') ?>
-			</div>
-		</div>
-		</section>
+			
+			<div class="row container my-5 text-center cajanoticias">
+				<div class="colxs-12 col-sm-4 col-md-4 py-3">
+					<i class="fas fa-thumbs-up noticia__icono"></i>
+					<span class="contador">0</span>
+				</div>
+				<div class="colxs-12 col-sm-4 col-md-4 py-3">
+					<i class="fas fa-thumbs-down noticia__icono"></i>
+					<span class="contador2">0</span>
+				</div>
+				<div class="colxs-12 col-sm-4 col-md-4 py-3 ">
+					<a href="blog.html"><button type="button" class="btn btn-info">Volver</button></a>
+				</div>
+			</div><!--fin row interno-->
+
+		</div><!--fin row-->
+	</section>
+		
 	<?php } ?>
 <?php } else { ?>
-	
-		
-		
-	
 <?php } wp_reset_query(); ?>
 
 <?php get_footer() ?>
